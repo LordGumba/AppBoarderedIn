abd1e465f107eb1bce71600ddc13786d4ca10687
LordGumba <rou21004@byui.edu>
2024-05-25 19:49:31 -0600

    hre

--- App.java
+++ App.java
@@ -1,3 +1,17 @@
-public class App {
 
+import java.awt.*;
+import javax.swing.JComponent;
+
+
+
+class ShapeDrawing extends JComponent {
+    int x = 1, y = 1;
+    
+    public void paint(Graphics g)
+    {
+        Graphics2D g2 = (Graphics2D) g;
+        x = x - 1;
+        y = y - 1;
+        g2.drawOval(x, y, 20, 20);
+  }
 }
